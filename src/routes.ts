5ff07fc17d0c48cba1b0f893979051994c115c42
Breno S F <BrenoSilvaFortunato@gmail.com>
2021-04-20 11:50:31 -0300

    create repository and controller

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsController } from "./controllers/SettingsController";
+import { SettingsRepository } from "./repositories/SettingsRepository";
+
+const routes = Router();
+
+const settingsController = new SettingsController();
+
+routes.post('/settings', settingsController.create );
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import './database';
+import { routes } from './routes'
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.get('/', (req, res) => {
     return res.json({
